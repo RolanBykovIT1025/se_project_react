0b19069b9e2207fbfdf8139d47ea89f6567a4ffb
Rolan Bykov <Rbykov30@gmail.com>
2025-07-08 15:03:34 -0400

    rendering/filtering cards

--- vite-project/src/components/Main/Main.jsx
+++ vite-project/src/components/Main/Main.jsx
@@ -1,15 +1,26 @@
 import WeatherCard from "../WeatherCard/WeatherCard";
+import ItemCard from "../itemCard/ItemCard";
+import { defaultClothingItems } from "../../utils/constants";
 
-function Main() {
-    return <main>
-        <WeatherCard />
-        <section className="cards">
-            <p className="cards__text">
-                Today is 75 &deg; F / You may want to wear:
-            </p>
-            {/* TODO -- ADD CARDS*/}
-        </section>
-        </main>;
+function Main({ weatherData }) {
+  return (
+    <main>
+      <WeatherCard />
+      <section className="cards">
+        <p className="cards__text">
+          Today is 75 &deg; F / You may want to wear:
+        </p>
+        <ul className="cards__list">
+          {defaultClothingItems.filter((item) => {
+            return item.weather === weatherData.type;
+          }).map((item) => {
+            return (<ItemCard key={item.id} item={item} />
+            )
+          })}
+        </ul>
+      </section>
+    </main>
+  );
 }
 
 export default Main;
